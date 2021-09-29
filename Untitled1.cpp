492c5b6c5ed7834fa84ed551f732c7f62470e951
jennyfier <91593636+jennyfier@users.noreply.github.com>
2021-09-29 13:18:12 +0530

    Add files via upload

---
+++ Untitled1.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int n, a, b,ca=0,cb=0;
+        cin >> n >> a >> b;
+        string s;
+        cin>>s;
+        for (int i = 0; i < n; i++)
+        {
+            if(s[i]=='0')
+            ca++;
+            else
+            cb++;
+        }
+        int ans=0;
+        ans=((ca*a)+(cb*b));
+        cout<<ans<<endl;
+    }
+    return 0;
+}
+
+
